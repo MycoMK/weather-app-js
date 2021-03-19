ae045e42f31f56d01fb0d47ca6ef69154277d16f
MICHAEL KINGS <kmichael381@gmail.com>
2021-03-19 07:45:36 -0700

    weather-api-data

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('connected');
+
+let cityName = 'Houston'
+const apikey = 'ff8345d57b34c925a8ba78c22f9b34dc'
+function fecthData(){
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}`)
+    .then(response => { return response.json()
+    }).then(data => { console.log(`data`,data)
+    })
+}
+
+fecthData();
