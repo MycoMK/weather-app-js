c25b54ea10aa59364575685049510a1743971b4e
MICHAEL KINGS <57077025+MycoMK@users.noreply.github.com>
2021-10-27 06:14:39 -0700

    edits

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// console.log('connected');
+console.log('connected');
 document.getElementById('getText').addEventListener('click', getText);
 function getText(){
     console.log('lets Gooo');
 }
 let cityName = 'California'
 const apikey = 'ff8345d57b34c925a8ba78c22f9b34dc'
-function fecthData(){
+function fetchData(){
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}`)
     .then(response => { return response.json()
     }).then(data => { console.log(`data`,data)
     })
+    
 }
-
-fecthData();
+fetchData()
